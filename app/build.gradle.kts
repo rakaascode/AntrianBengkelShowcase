16460b1b012caf71b53f1fdd556a318cf11b3c18
Raka Saputra <emailgithubmu@gmail.com>
2026-08-29 03:35:50 +0700

    chore: bump versionCode to 7 and versionName to 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.4.1"
+        versionCode = 7
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
